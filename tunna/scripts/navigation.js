972ee9a753265b1e9e35af1f4fd3604d3cb3a170
fernandoscjunior <fernandojrsud@icloud.com>
2026-01-19 13:21:17 -0300

    finished mobile view for home page

---
+++ tunna/scripts/navigation.js
@@ -0,0 +1,11 @@
+//Getting hamburger button on header
+const navButton = document.querySelector('#ham-btn');
+
+//Adding event listener on hambuger button
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
+});
+
+//getting navigation menu
+const navBar = document.querySelector('#nav-bar');
